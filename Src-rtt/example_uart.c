e573fba10d21309bfac67ed300f85dc315e28e07
SuWeipeng <weipeng.su@outlook.com>
2019-10-30 15:41:13 +0800

    解决官方例子在 msh 下输入两次 uart_sample 之后出错的问题。

--- Src-rtt/example_uart.c
+++ Src-rtt/example_uart.c
@@ -27,47 +27,44 @@ static void serial_thread_entry(void *parameter)
     }
 }
 
+int example_uart_init(void)
+{
+  serial = rt_device_find(SAMPLE_UART_NAME);
+  if (!serial)
+  {
+    rt_kprintf("find %s failed!\n", SAMPLE_UART_NAME);
+    return RT_ERROR;
+  }
+  
+  rt_err_t ret = RT_EOK;
+  
+  rt_sem_init(&rx_sem, "rx_sem", 0, RT_IPC_FLAG_FIFO);
+  
+  rt_device_open(serial, RT_DEVICE_FLAG_INT_RX);
+  
+  rt_device_set_rx_indicate(serial, uart_input);
+  
+  rt_thread_t thread = rt_thread_create("serial", serial_thread_entry, RT_NULL, 1024, 25, 10);
+  
+  if (thread != RT_NULL)
+  {
+    rt_thread_startup(thread);
+  }
+  else
+  {
+    ret = RT_ERROR;
+  }
+    
+  return ret;
+}
+
 static int uart_sample(int argc, char *argv[])
 {
     rt_err_t ret = RT_EOK;
-    char uart_name[RT_NAME_MAX];
     char str[] = "hello RT-Thread!\r\n";
-
-    if (argc == 2)
-    {
-        rt_strncpy(uart_name, argv[1], RT_NAME_MAX);
-    }
-    else
-    {
-        rt_strncpy(uart_name, SAMPLE_UART_NAME, RT_NAME_MAX);
-    }
-
-    serial = rt_device_find(uart_name);
-    if (!serial)
-    {
-        rt_kprintf("find %s failed!\n", uart_name);
-        return RT_ERROR;
-    }
-
-    rt_sem_init(&rx_sem, "rx_sem", 0, RT_IPC_FLAG_FIFO);
-
-    rt_device_open(serial, RT_DEVICE_FLAG_INT_RX);
-
-    rt_device_set_rx_indicate(serial, uart_input);
-
+    
     rt_device_write(serial, 0, str, (sizeof(str) - 1));
-
-    rt_thread_t thread = rt_thread_create("serial", serial_thread_entry, RT_NULL, 1024, 25, 10);
-
-    if (thread != RT_NULL)
-    {
-        rt_thread_startup(thread);
-    }
-    else
-    {
-        ret = RT_ERROR;
-    }
-
+    
     return ret;
 }
 MSH_CMD_EXPORT(uart_sample, uart device sample);
